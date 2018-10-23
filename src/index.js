aa46f0f42d71271eba2452ef1a085e6784e2db03
derrick b <dbozich1@gmail.com>
2018-10-23 01:38:38 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ export default Class Invitation extends React.Component{
   render(){
     return(
       <div className="invitation">
-        <h1> You've been invited! <h1/>
+        <h1> Youve been invited! <h1/>
+        <p>{this.}</p>
       </div>
     )
   }
